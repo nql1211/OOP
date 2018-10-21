3c23e8aca4a6ded88f79b99e833209a52e8d002d
ngobathieu <ngobathieu@github.com>
2018-10-21 20:08:57 +0700

    BTL

---
+++ tudien/src/tudien/Dictionary.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tudien;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Genius
+ */
+@SuppressWarnings("serial")
+class Dictionary extends Vector < Word >{
+
+    public Dictionary(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    public Dictionary() {
+    }    
+}
